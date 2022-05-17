a532374c4ed5d396624bed6bb1021c9ed1a1ea44
Sandeep <skumar8work@gmail.com>
2022-05-17 16:50:14 +0530

    groups views and templates setup

--- codingsocial/groups/views.py
+++ codingsocial/groups/views.py
@@ -1,3 +1,19 @@
+from calendar import c
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                            PermissionRequiredMixin, )
+from django.urls import reverse
+from django.views import generic
+
+from .models import Group,GroupMember
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields = ('name','description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
