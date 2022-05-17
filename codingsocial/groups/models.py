d341e8645caf8cd798ed1511ee15d65b6c0e3a8f
Sandeep <skumar8work@gmail.com>
2022-05-17 13:45:52 +0530

    posts and groups app models created

--- codingsocial/groups/models.py
+++ codingsocial/groups/models.py
@@ -1,3 +1,46 @@
+from atexit import register
+from audioop import reverse
 from django.db import models
+from django.utils.text import slugify #slugify Converts to lowercase, removes non-word characters (alphanumerics and underscores) and converts spaces to hyphens
 
+#groups models.py file
 # Create your models here.
+import misaka # pip install misaka 
+
+from django.contrib.auth import get_user_model
+User = get_user_model
+
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    name = models.CharField(max_length=255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    description = models.TextField(blank=True,default='')
+    description_html = models.TextField(editable=False,default='',blank=True)
+    members = models.ManyToManyField,through='GroupMember'
+
+    def __str__(self):
+        return self.name
+        
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single',kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='memberships')
+    user = models.ForeignKey(User,related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        unique_together =('group','user')
