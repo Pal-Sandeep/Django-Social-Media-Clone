c4b528405fe3d847481c32031799d028fb9dd246
Sandeep <skumar8work@gmail.com>
2022-05-18 13:26:34 +0530

    registered groups app models in admin.py file

--- codingsocial/groups/admin.py
+++ codingsocial/groups/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMember
+    
+admin.site.register(models.Group)
